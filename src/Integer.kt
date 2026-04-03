d431a78151d35692f66066e6a67047f107bb2732
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2026-04-02 23:14:15 -0500

    more excercise

---
+++ src/Integer.kt
@@ -0,0 +1,13 @@
+fun main(parameters:Array<String>){
+    println("Define different variables with respect values")
+    val age : Int  // In this part  we cannot  change the value of the variable when use "val"
+    age =25
+
+
+    var month: Int  // In other part  if we can change use "var"
+    month = 12
+    month = 13
+
+
+
+}
