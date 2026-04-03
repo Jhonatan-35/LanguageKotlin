fc3ffec4ffb25d0442f71667a41cbbea873cee36
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2026-04-03 18:02:55 -0500

    saad

---
+++ src/Square.kt
@@ -0,0 +1,6 @@
+fun main (parameter : Array<String>){
+    val side = 50
+    val perimeter = side * 4
+    val surface  = side * side
+    print("The perimeter of a square with a side $side is $perimeter and its surface is $surface")
+}
